34dc1c775e4808664dcdb5824b8f2ed5f12e94a1
Marco La Rosa <marco@larosa.org.au>
2013-08-10 21:35:25 +1000

    add jsonp renderer and route for graph build status

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.renderers import JSONP
 from sqlalchemy import engine_from_config
 
 from .models import (
@@ -14,8 +15,10 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    config.add_renderer('jsonp', JSONP(param_name='callback'))
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('site_graph', '/site/{code}')
     config.add_route('entity_graph', '/entity/{id}')
+    config.add_route('status', '/status')
     config.scan()
     return config.make_wsgi_app()
