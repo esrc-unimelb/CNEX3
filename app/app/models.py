c18fa80ec48bbfe4e1ec67adc8ab206f96bf4d1c
Marco La Rosa <marco@larosa.org.au>
2013-08-29 13:44:36 +1000

    playing with the models

--- app/app/models.py
+++ app/app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import (
     Integer,
     String,
     Text,
+    Index,
+    Date
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,10 +19,20 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
 class Progress(Base):
     __tablename__ = 'progress'
     id = Column(Integer, primary_key=True)
-    total = Column(String)
-    processed = Column(String)
-    uuid = Column(String, index=True)
+    site = Column(String, index=True)
+    session_id = Column(String, index=True)
+    processed = Column(Integer)
+    total = Column(Integer)
+
+class Graph(Base):
+    __tablename__ = 'graph'
+    id = Column(Integer, primary_key=True)
+    site = Column(String, index=True)
+    session_id = Column(String, index=True)
+    graph = Column(Text)
+    site_name = Column(String)
+
+ 
