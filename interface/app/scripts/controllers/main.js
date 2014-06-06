d57dac8bd3ae5d6fba1b945b273eac4e89299378
Marco La Rosa <m@lr.id.au>
2014-06-06 16:05:18 +1000

    home page sorted
    list of available sites / graphs online

--- interface/app/scripts/controllers/main.js
+++ interface/app/scripts/controllers/main.js
@@ -3,9 +3,14 @@
 angular.module('interfaceApp')
   .controller('MainCtrl', [ '$scope', 'configuration', '$http', function ($scope, configuration, $http) {
 
-      var q = configuration[configuration.service];
+      var q = configuration[configuration.service] + '?callback=JSON_CALLBACK';
       console.log(q);
       $http.jsonp(q).then(function(d) {
-        console.log(d);
+          console.log(d.data.sites);
+          $scope.sites = [];
+          angular.forEach(d.data.sites, function(v, k) {
+              $scope.sites.push([k, v]);
+          });
+          console.log($scope.sites);
       });
   }]);
