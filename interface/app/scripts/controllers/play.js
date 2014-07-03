d762633a672fc68cb21b5f5678efb3bf91be8e45
Marco La Rosa <m@lr.id.au>
2014-07-04 09:35:50 +1000

    tinkering with date visualisation code

--- interface/app/scripts/controllers/play.js
+++ interface/app/scripts/controllers/play.js
@@ -3,109 +3,124 @@
 angular.module('interfaceApp')
   .controller('PlayCtrl', [ '$scope', '$window', function ($scope, $window) {
 
-      $scope.items = [ 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10' ];
+      var w = 500;
+      var h = 400;
 
-      var w = $window.innerWidth;
-      var h = $window.innerHeight;
+      var dataset = [
+          { 'name': 'AE1', 'df': 2010, 'dt': null },
+          { 'name': 'AE2', 'df': 2005, 'dt': 2011 },
+          { 'name': 'AE3', 'df': null, 'dt': 2011 },
+      ]
+
+      $scope.points = [];
+      $scope.ranges = [];
+      angular.forEach(dataset, function(v, k) {
+          if (v.df !== null && v.dt !== null) {
+              $scope.ranges.push(v);
+          } else {
+              if (v.df !== null || v.dt !== null) {
+                  $scope.points.push(v);
+              }
+          }
+      });
+      console.log($scope.ranges);
+      console.log($scope.points);
 
       d3.select('svg').remove();
       var color = d3.scale.category20();
-      var nodes = [ { name: 'E0' }];
-      var links = [];
 
-      var force = d3.layout.force()
-            .nodes(nodes)
-            .links(links)
-            .charge(-1000)
-            .linkDistance(100)
-            .linkStrength(1)
-            .size([w, h])
-            .on('tick', tick);
+
+      var t1 = new Date(2000, 0, 1);
+      var t2 = new Date(2014, 12, 31);
+
+      var xScale = d3.time.scale()
+                .domain([t1, t2])
+                .range([30, (w - 30)]);
+
+      var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .ticks(d3.time.years)
+                    .tickSize(6, 0)
+                    .orient("bottom");
+
+      var yScale = d3.scale.linear()
+                     .domain([0, 1])
+                     .range([30, (h-100)]);
+
+      /*
+      var yAxis = d3.svg.axis()
+                    .scale(yScale)
+                    .ticks(dataset.length)
+                    .orient('left');
+      */
 
       var svg = d3.select('#graph')
             .append('svg')
             .attr('width', w)
-            .attr('height', h)
-            .attr('viewBox', '0 0 ' + w + ' ' + h)
-            .attr('preserveAspectRatio', 'xMidYMid meet')
-            .call(d3.behavior.zoom().scaleExtent([0,8]).on('zoom', redraw))
-            .append('g');
+            .attr('height', h);
 
-      svg.append("g").attr("class", "links");
-      svg.append("g").attr("class", "nodes");
+      var gx = svg.append('g')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(0,' + (h - 100) + ')')
+                  .call(xAxis);
 
-      var node = svg.select(".nodes").selectAll(".node");
-      var link = svg.select(".links").selectAll(".link");
+      /*
+      var gy = svg.append('g')
+                  .attr('class', 'axis')
+                  .attr('transform', 'translate(30, 0)')
+                  .call(yAxis);
+      */
 
-      $scope.removeOne = function() {
-          var node = nodes.splice(0,1);
-          links.splice(0,1);
-          $scope.items.push(node[0].name);
-          update();
-      };
+      var rect = svg.selectAll('rect').data($scope.ranges);
+      rect.enter()
+          .append('rect')
+          .attr('class', 'rect')
+          .attr('x', function(d) {
+              var date = Date.parse(d.df);
+              console.log(d.name, 'x', xScale(date));
+              return xScale(date);
+          })
+          .attr('y', function(d, i) {
+              console.log(d.name, 'y', yScale(Math.random()));
+              return yScale(Math.random());
+          })
+          .attr('width', function(d) {
+              if (d.dt === undefined || d.dt === '') {
+                  return 100;
+              } else {
+                  var df = Date.parse(d.df);
+                  var dt = Date.parse(d.dt);
+                  return xScale(dt) - xScale(df);
+              }
+          })
+          .attr('height', '10')
+          .attr('fill', 'grey');
 
-      $scope.addOne = function() {
-          var n = $scope.items.shift();
-          var link = { source: nodes.length-1, target: nodes.length };
-          nodes.push( { name: n });
-          links.push(link);
-          update();
-      }
-      $scope.addTen = function() {
-        for (var i=0; i<5; i++) {
-            $scope.addOne();
-        }
-      }
-
-      var update = function() {
-          force.start();
-
-          console.log(links);
-          link = link.data(links);
-
-          link.enter()
-              .append('line')
-              .attr('class', 'link')
-              .attr('stroke', '#ccc')
-              .attr('stroke-width', 2);
-          link.exit().remove();
-
-          console.log(nodes);
-          node = node.data(nodes);
-
-          node.enter()
+        var circle = svg.selectAll('circle').data($scope.points);
+        circle.enter()
               .append('circle')
-              .attr('class', 'node')
-              .attr('r', 10)
-              .attr('fill', function(d) { return color(Math.random()); });
-          node.exit().remove();
-
-      }
-
-      function tick() {
-          link.attr('x1', function(d) { return d.source.x; })
-              .attr('y1', function(d) { return d.source.y; })
-              .attr('x2', function(d) { return d.target.x; })
-              .attr('y2', function(d) { return d.target.y; });
-
-          node.attr('transform', function(d) {
-            return 'translate(' + d.x + ',' + d.y + ')';
-          });
-
-      }
-
-      function redraw() {
-          svg.attr('transform', 'translate(' + d3.event.translate + ')' + ' scale(' + d3.event.scale + ')');
-      }
-
-      function scale(d) {
-          var log = d3.scale.log().range([10,30]);
-          if (d.connections == 0) {
-            return log(1);
-          } else {
-              return log(d.connections);
-          }
-      }
+              .attr('class', 'circle')
+              .attr('cx', function(d) {
+                  if (d.df !== null) {
+                      var date = Date.parse(d.df);
+                  } else {
+                      var date = Date.parse(d.dt);
+                  }
+                  console.log(d.name, 'x', xScale(date));
+                  return xScale(date);
+            })
+              .attr('cy', function(d, i) {
+                  console.log(d.name, 'y', yScale(Math.random()));
+                  return yScale(Math.random());
+              })
+              .attr('r', '5')
+              .attr('fill', function(d) {
+                  if (d.df !== null) {
+                      return 'red';
+                  } else {
+                      return 'green';
+                  }
+              });
 
   }]);
 
