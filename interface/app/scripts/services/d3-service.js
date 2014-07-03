11953c64b074644410299666e56caee449abc73a
Marco La Rosa <m@lr.id.au>
2014-07-04 09:44:26 +1000

    add d3 service; handles all element updates

---
+++ interface/app/scripts/services/d3-service.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('interfaceApp')
+  .service('D3Service', function D3Service() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
