46d093d48b24fba69e853761e62fcf6f5b6d5e7f
Marco La Rosa <m@lr.id.au>
2015-02-11 15:47:40 +1100

    Add colour map by type

--- interface/app/scripts/services/configuration.js
+++ interface/app/scripts/services/configuration.js
@@ -7,6 +7,19 @@ angular.module('interfaceApp')
       'service': 'development',
       'solr': 'https://solr.esrc.unimelb.edu.au/ESRC/select',
 
+      'colours': {
+          'person': '#d62728',
+          'corporatebody': '#ffbb78',
+          'event': '#1f77b4',
+          'concept': '#aec7e8',
+          'place': '#ff9896',
+          'culturalartefact': '#9467bd',
+          'published': '#98df8a',
+          'archival': '#2ca02c',
+          'digitalObject': '#ff7f0e'
+
+      },
+
       'fill': {
           'contextNode': 'green',
           'contextNeighbourDefault': 'orange',
@@ -16,7 +29,7 @@ angular.module('interfaceApp')
 
       'opacity': {
           'default': '1',
-          'unselected': '0.5'
+          'unselected': '0.3'
       },
 
       'stroke': {
