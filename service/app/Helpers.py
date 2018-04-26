2191de0de84ab5ea69bf59f828bbb155f3433412
Chris Kirk <chris@kirin.gdhgps.com>
2018-04-26 12:07:29 +1000

    fix problem where an attribute is not found in the XML and lxml raises a KeyError exception now returns empty value

--- service/app/Helpers.py
+++ service/app/Helpers.py
@@ -49,13 +49,20 @@ def get(tree, path, attrib=None, element=None, aslist=None):
 
     # return the requested attribute
     elif attrib is not None:
-        return result[0].attrib[attrib]
+        try:
+            attribValue = result[0].attrib[attrib]
+            return attribValue
+        except:
+# handle error where the attribute is not found in the XML
+            log.error(path)
+            log.error(tree.xpath(path, namespaces={ 'e': 'urn:isbn:1-931666-33-4' }))
+            return []    
+        #return result[0].attrib[attrib]
         #return tree.xpath(path, namespaces={ 'e': 'urn:isbn:1-931666-33-4' })[0].attrib[attrib]
 
     # otherwise - return the text content of the node
     else:
         try:
-            #print "**", result, len(result)
             if len(result) == 1:
                 return result[0].text
             else:
