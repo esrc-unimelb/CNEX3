2eba7548feaf8b863395534d7382ea44ee854b9d
Peter Tonoli <peter+github@metaverse.org>
2018-04-27 14:11:11 +1000

    Document experimental/clique_status

--- service/app/experimental.py
+++ service/app/experimental.py
@@ -36,21 +36,22 @@ def clique_status(request):
     claims, site_data = verify_access(request, site=site)
 
     doc = db.network.find_one({ 'site': site, 'graph_type': graph_type })
+    # if there a graph in Mongo already?
     if doc is not None:
         graph_data = doc['graph_data']
         doc = db.network_progress.remove({ 'site': site })
 
-#        G = json_graph.node_link_graph(graph_data, directed=False, multigraph=False)
-#        if not nx.is_connected(G):
-#            components = nx.connected_component_subgraphs(G)
-#            (index, G) = max(enumerate(components), key = lambda tup: len(tup[1]))
-#        return { 'total': None, 'processed': None, 'graph': graph_data, 'center': nx.center(G) }
+#       # Convert from the directed graph in Mongo, to undirected, for find_cliques, as find_cliques is
+        # incompatible with directed graphs
         clique_data = json_graph.node_link_graph(graph_data, directed=False, multigraph=False)
         cliques = nx.find_cliques(clique_data)
+
+        # Find cliques with 3 or more entities, discard the rest
         cliques3 = [clq for clq in cliques if len(clq) >=3]
         return { 'total': None, 'processed': None, 'cliques': cliques3 }
         #return { 'total': doc['total'], 'processed': doc['processed'] }
     else:
+        # no graph in Mongo, lets generate one
         doc = db.network_progress.find_one({ 'site': site })
         return { 'total': doc['total'], 'processed': doc['processed'] }
 
